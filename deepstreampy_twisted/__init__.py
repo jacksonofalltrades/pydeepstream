ff1477c165718a4e86b7c4751ad7db3e39f52806
Will Crawford <378681+sapid@users.noreply.github.com>
2017-12-15 00:23:08 -0800

    __init__ for new package

--- deepstreampy_twisted/__init__.py
+++ deepstreampy_twisted/__init__.py
@@ -2,12 +2,20 @@
 from __future__ import absolute_import, division, print_function, with_statement
 from __future__ import unicode_literals
 
-#from deepstreampy_twisted.client import connect
-from deepstreampy_twisted import constants, message, protocol
+from tornado.platform.twisted import TwistedIOLoop
+TwistedIOLoop().install() # TODO: Is this compatible with alternative reactors?
+from twisted.logger import Logger
+log = Logger()
+import txaio
+txaio.use_twisted()
+from deepstreampy import constants, message
+import deepstreampy_twisted
+from deepstreampy_twisted import protocol
 from deepstreampy_twisted.protocol import DeepstreamFactory, DeepstreamProtocol
 
 
-__all__ = ["DeepstreamClient, DeepstreamFactory, DeepstreamProtocol, constants"]
+
+__all__ = ["DeepstreamClient, DeepstreamFactory, DeepstreamProtocol"]
 
 
 version = "0.2.0"
